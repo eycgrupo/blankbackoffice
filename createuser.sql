10280913db132bfdd267da22dc527d5c177c49a6
Mario Estrada <marioestradarosa@yahoo.com>
2023-06-12 18:29:17 -0600

    feat add(webapp) añade logos nuevos
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- createuser.sql
+++ createuser.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE hello;
-CREATE USER "hello2"@"localhost" IDENTIFIED WITH mysql_native_password BY 'HelloWorld99$90';
-GRANT ALL PRIVILEGES ON hello.* TO 'hello2'@'localhost';
+CREATE USER "hello2"@"172.17.0.1" IDENTIFIED WITH mysql_native_password BY 'HelloWorld99$90';
+GRANT ALL PRIVILEGES ON hello.* TO 'hello2'@'172.17.0.1';
