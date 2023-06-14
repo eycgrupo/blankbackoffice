095303e1b3963f5ad9144eb90c93f8b36ecd12d6
Mario Estrada <marioestradarosa@yahoo.com>
2023-06-14 16:46:40 -0600

    feat add pagos
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 ./gradlew clean
 rm -fr bin
-mysql -uhello2 -h127.0.0.1 --port=6603  -pHelloWorld99$90 hello < ./cleanMeta.sql
+mysql -uroot -h127.0.0.1 --port=6603  hello < ./cleanMeta.sql
 ./gradlew --no-daemon run --port 8080
