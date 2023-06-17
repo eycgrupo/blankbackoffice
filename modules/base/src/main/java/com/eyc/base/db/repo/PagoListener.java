3514c8a623590c891a2a5159a9e162864aab4abc
Mario Estrada <marioestradarosa@yahoo.com>
2023-06-16 18:55:15 -0600

    feat add validaciones
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- modules/base/src/main/java/com/eyc/base/db/repo/PagoListener.java
+++ modules/base/src/main/java/com/eyc/base/db/repo/PagoListener.java
@@ -1,12 +1,18 @@
 package com.eyc.base.db.repo;
 
 import com.eyc.base.db.Pago;
+import com.eyc.base.db.Cliente;
 import com.axelor.i18n.I18n;
 import javax.persistence.PostPersist;
-
-public class PagoListener {
+ 
+ public class PagoListener {
+ 
   @PostPersist
   private void pluto(Pago pago) {
-     System.out.println("Pago guardado");
+
+    Cliente cliente = pago.getCliente();
+  
+     System.out.println("Pago guardado para cliente " 
+     + cliente.getNombreCompleto().toString());
   }
 } 
