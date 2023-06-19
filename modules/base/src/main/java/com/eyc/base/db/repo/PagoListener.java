022da8912ef4918291e4fbb42d48d91356f0f3e3
Mario Estrada <marioestradarosa@yahoo.com>
2023-06-19 17:42:15 -0600

    agrega reducir saldo en pago
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- modules/base/src/main/java/com/eyc/base/db/repo/PagoListener.java
+++ modules/base/src/main/java/com/eyc/base/db/repo/PagoListener.java
@@ -1,18 +1,17 @@
 package com.eyc.base.db.repo;
-
+ 
 import com.eyc.base.db.Pago;
 import com.eyc.base.db.Cliente;
 import com.axelor.i18n.I18n;
 import javax.persistence.PostPersist;
  
  public class PagoListener {
- 
+   
   @PostPersist
   private void pluto(Pago pago) {
 
     Cliente cliente = pago.getCliente();
-  
-     System.out.println("Pago guardado para cliente " 
-     + cliente.getNombreCompleto().toString());
+    cliente.setSaldo(cliente.getSaldo().subtract(pago.getMonto()));
+
   }
 } 
