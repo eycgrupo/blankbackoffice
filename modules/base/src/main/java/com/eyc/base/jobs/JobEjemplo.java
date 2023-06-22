08cc465173327325c81647a56cf5c458814a75f8
Mario Estrada <marioestradarosa@yahoo.com>
2023-06-21 18:18:36 -0600

    feat adds auditTrails and JobService
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

---
+++ modules/base/src/main/java/com/eyc/base/jobs/JobEjemplo.java
@@ -0,0 +1,27 @@
+
+package com.eyc.base.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+ public class JobEjemplo implements Job {
+
+  @Override
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+    JobDetail detail = context.getJobDetail();
+    JobDataMap data = context.getJobDetail().getJobDataMap();
+
+    String name = detail.getKey().getName();
+    String desc = detail.getDescription();
+
+    System.err.println("Job disparado: " + name + " (" + desc + ")");
+    if (data != null && data.size() > 0) {
+      for (String key : data.keySet()) {
+        System.err.println("    " + key + " = " + data.getString(key));
+      }
+    }
+  }
+}
