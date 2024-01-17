b7e1111eb415e042a8eda80ae81a2061ecb1bd64
Mario Estrada <marioestradarosa@yahoo.com>
2024-01-17 17:25:55 -0600

    feat add(base) implements customer view and menu
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 ./gradlew clean
 rm -fr bin
-mysql -uhello2 -h127.0.0.1 --port=3306  -pHelloWorld99$90 hello < ./cleanMeta.sql
+mysql -uhello2 -h127.0.0.1 --port=3306  -pHelloWorld9990 hello < ./cleanMeta.sql
 ./gradlew --no-daemon run --port 8080
+
+
+ 
