8a27606108578595ef5bdc31df580831d1d3fee0
Mario Estrada <marioestradarosa@yahoo.com>
2024-01-18 15:54:37 -0600

    feat add(base) adds invoice and its detail domain
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./gradlew clean
 rm -fr bin
-mysql -uhello2 -h127.0.0.1 --port=3306  -pHelloWorld9990 hello < ./cleanMeta.sql
+mysql -uhello2 -h127.0.0.1 --port=6603  -pHelloWorld9990 hello < ./cleanMeta.sql
 ./gradlew --no-daemon run --port 8080
 
 
