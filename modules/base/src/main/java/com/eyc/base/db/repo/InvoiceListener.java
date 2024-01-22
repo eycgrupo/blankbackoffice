06a5fa64db3ac168f5510c685704ec42945289c8
Mario Estrada <marioestradarosa@yahoo.com>
2024-01-22 16:25:55 -0600

    fix (base) invoice to customer balance
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- modules/base/src/main/java/com/eyc/base/db/repo/InvoiceListener.java
+++ modules/base/src/main/java/com/eyc/base/db/repo/InvoiceListener.java
@@ -2,14 +2,25 @@ package com.eyc.base.db.repo;
  
 import com.eyc.base.db.Invoice;
 import com.eyc.base.db.Customer;
-import com.axelor.i18n.I18n;
+import com.eyc.base.db.InvoiceDetail;
 import javax.persistence.PostPersist;
+import com.axelor.i18n.I18n;
+import java.util.List;
+import java.math.BigDecimal; 
+import java.util.Objects;
  
  public class InvoiceListener {
    
   @PostPersist
   private void updateTheBalance(Invoice invoice) {
     Customer theCustomer = invoice.getCustomer();
-    theCustomer.setCurrentBalance(theCustomer.getCurrentBalance().add(invoice.getInvoiceTotal()));
+    List<InvoiceDetail> invoiceDetail  = invoice.getInvoiceItems();
+    BigDecimal totalDetail = new BigDecimal("0.0");
+
+    for (int i = 0; i < invoiceDetail.size(); i++) {
+     totalDetail = totalDetail.add(invoiceDetail.get(i).getTotal());
+    }
+    
+    theCustomer.setCurrentBalance(theCustomer.getCurrentBalance().add(totalDetail));
   }
 } 
